06fb059549d5318668dde49a756b4434e9480835
MetalYos <metalyos@gmail.com>
2023-08-04 01:23:28 +0300

    Removed sudo from i3 script

--- PostInstall/install_i3.sh
+++ PostInstall/install_i3.sh
@@ -11,41 +11,41 @@ username=${1}
 
 # Install graphical environment and i3
 echo -e "${BYellow}[ * ]Install graphical environment and i3${End_Colour}"
-sudo pacman -S xorg-server xorg-apps xorg-xinit --noconfirm
-sudo pacman -S i3-gaps i3blocks i3lock i3status numlockx --noconfirm
+pacman -S xorg-server xorg-apps xorg-xinit --noconfirm
+pacman -S i3-gaps i3blocks i3lock i3status numlockx --noconfirm
 
 # Install display manager
 echo -e "${BYellow}[ * ]Install display manager${End_Colour}"
-sudo pacman -S lightdm lightdm-gtk-greeter --needed --noconfirm
-sudo systemctl enable lightdm
+pacman -S lightdm lightdm-gtk-greeter --needed --noconfirm
+systemctl enable lightdm
 
 # Install some basic fonts
 echo -e "${BYellow}[ * ]Install some basic fonts${End_Colour}"
-sudo pacman -S noto-fonts ttf-ubuntu-font-family ttf-dejavu ttf-freefont --noconfirm
-sudo pacman -S ttf-liberation ttf-droid ttf-roboto terminus-font --noconfirm
+pacman -S noto-fonts ttf-ubuntu-font-family ttf-dejavu ttf-freefont --noconfirm
+pacman -S ttf-liberation ttf-droid ttf-roboto terminus-font --noconfirm
 
 # Install some useful tools on i3
 echo -e "${BYellow}[ * ]Install some useful tools on i3${End_Colour}"
-sudo pacman -S kitty ranger rofi dmenu --needed --noconfirm
+pacman -S kitty ranger rofi dmenu --needed --noconfirm
 
 # Install some GUI programs
 echo -e "${BYellow}[ * ]Install some GUI programs${End_Colour}"
-sudo pacman -S firefox vlc --needed --noconfirm
+pacman -S firefox vlc --needed --noconfirm
 
 # Install zsh
 echo -e "${BYellow}[ * ]Install zsh${End_Colour}"
-sudo pacman -S zsh git --noconfirm
+pacman -S zsh git --noconfirm
 echo Y exit | sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
 
 # Install LX LXAppearance and themes
 echo -e "${BYellow}[ * ]Install LX LXAppearance and themes${End_Colour}"
-sudo pacman -S lxappearance arc-gtk-theme papirus-icon-theme --noconfirm
+pacman -S lxappearance arc-gtk-theme papirus-icon-theme --noconfirm
 
 # Customize LightDM
 echo -e "${BYellow}[ * ]Customize LightDM${End_Colour}"
-sudo sed -i 's/#background=/background=#2f343f/g' /etc/lightdm/lightdm-gtk-greeter.conf
-sudo sed -i 's/#icon-theme-name=/icon-theme-name=Papirus-Dark/g' /etc/lightdm/lightdm-gtk-greeter.conf
-sudo sed -i 's/#theme-name=/theme-name=Arc-Dark/g' /etc/lightdm/lightdm-gtk-greeter.conf
+sed -i 's/#background=/background=#2f343f/g' /etc/lightdm/lightdm-gtk-greeter.conf
+sed -i 's/#icon-theme-name=/icon-theme-name=Papirus-Dark/g' /etc/lightdm/lightdm-gtk-greeter.conf
+sed -i 's/#theme-name=/theme-name=Arc-Dark/g' /etc/lightdm/lightdm-gtk-greeter.conf
 
 # Install Kitty themes
 echo -e "${BYellow}[ * ]Install Kitty themes${End_Colour}"
