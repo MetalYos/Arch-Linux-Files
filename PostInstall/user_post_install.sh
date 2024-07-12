a04add1ad7b251b8255be5bdb96fa65f77ea9c67
MetalYos <metalyos@gmail.com>
2024-07-12 20:26:40 +0300

    Fixed pipewire conflict. Fixed ordering.

--- PostInstall/user_post_install.sh
+++ PostInstall/user_post_install.sh
@@ -12,23 +12,28 @@ password=${2}
 
 function InstallAurHelper() {
 	echo -e "${BYellow}[ * ]Install the YAY AUR helper${End_Colour}"
-    cur_dir=pwd
+	cur_dir=pwd
 	cd /home/${username}/Downloads
+	echo "${password}" | sudo -S pacman -S git --noconfirm
 	git clone https://aur.archlinux.org/yay.git
 	cd yay
 	makepkg -si --noconfirm
 	cd ..
 	rm -Rf yay
-    cd ${cur_dir}
+	cd ${cur_dir}
 }
 
 function InstallAdditionalPackages() {
 	# Install other useful packages
 	echo -e "${BYellow}[ * ]Install useful packages${End_Colour}"
 	echo "${password}" | sudo -S pacman-key --init
-	echo "${password}" | sudo -S pacman -S timeshift dosfstools ntfs-3g iw wpa_supplicant dialog intel-ucode lshw unzip htop wget openssh git gdb valgrind man tldr stow bash-completion reflector mpv fastfetch transmission-gtk --noconfirm
+	echo "${password}" | sudo -S pacman -S timeshift dosfstools ntfs-3g iw wpa_supplicant dialog intel-ucode lshw unzip htop wget openssh git gdb valgrind man tldr stow bash-completion reflector mpv fastfetch transmission-gtk meld --noconfirm
+}
 
-    yay -S timeshift-autosnap sioyek amberol --noconfirm
+function InstallAdditionalAurPackages() {
+	echo -e "${BYellow}[ * ]Install useful AUR packages${End_Colour}"
+	yay -S timeshift-autosnap sioyek --noconfirm
+	# yay -S amberol --noconfirm
 }
 
 function EnableAutoUsbMounting() {
@@ -37,21 +42,26 @@ function EnableAutoUsbMounting() {
 	echo "${password}" | sudo -S pacman-key --init
 	echo "${password}" | sudo -S pacman -S udisks2 udiskie --noconfirm
 
-    echo -e "${BYellow}[ * ]Copying polkit rule for USB storage automounting${End_Colour}"
-    cur_dir=pwd
-    cd /PostInstall
-    cp 10-udisks2.rules /etc/polkit-1/rules.d/
-    cd ${cur_dir}
-
+	echo -e "${BYellow}[ * ]Copying polkit rule for USB storage automounting${End_Colour}"
+	cur_dir=pwd
+	cd /PostInstall
+	cp 10-udisks2.rules /etc/polkit-1/rules.d/
+	cd ${cur_dir}
 }
 
 function InstallPipewireAudio() {
 	echo -e "${BYellow}[ * ]Install Pipewire packages${End_Colour}"
-	echo "${password}" | sudo -S pacman -S pipewire wireplumber pipewire-alsa pipewire-pulse pipewire-jack dkms sof-firmware volumeicon alsa-utils --noconfirm
+
+	# Install all relevant Pipewrire packages
+	echo "${password}" | sudo -S pacman -Syu pipewire wireplumber pipewire-alsa pipewire-pulse pipewire-jack lib32-pipewire-jack dkms sof-firmware volumeicon alsa-utils --noconfirm
+	read -p "${BYellow}Press any key to continue...${End_Colour}"
+
 	echo -e "${BYellow}[ * ]Enable Pipewire user units${End_Colour}"
 	systemctl --user enable --now pipewire.socket
 	systemctl --user enable --now pipewire-pulse.socket
 	systemctl --user enable --now wireplumber.service
+	echo -e "${BYellow}[ * ]Install the YAY AUR helper${End_Colour}"
+	read -p "${BYellow}Press any key to continue...${End_Colour}"
 }
 
 function InstallDisplayServer() {
@@ -65,12 +75,12 @@ function InstallDisplayManager() {
 	sudo systemctl enable sddm.service
 
 	echo -e "${BYellow}[ * ]Copy default sddm config file to local folder${End_Colour}"
-    echo "${password}" | sudo -S mkdir -p /etc/sddm.conf.d
-    echo "${password}" | sudo -S cp /usr/lib/sddm/sddm.conf.d/default.conf /etc/sddm.conf.d/sddm.conf
+	echo "${password}" | sudo -S mkdir -p /etc/sddm.conf.d
+	echo "${password}" | sudo -S cp /usr/lib/sddm/sddm.conf.d/default.conf /etc/sddm.conf.d/sddm.conf
 
 	echo -e "${BYellow}[ * ]Install SDDM Astronaut theme manager${End_Colour}"
-    echo "${password}" | sudo -S git clone https://github.com/keyitdev/sddm-astronaut-theme.git /usr/share/sddm/themes/sddm-astronaut-theme
-    echo "${password}" | sudo -S cp /usr/share/sddm/themes/sddm-astronaut-theme/Fonts/* /usr/share/fonts/
+	echo "${password}" | sudo -S git clone https://github.com/keyitdev/sddm-astronaut-theme.git /usr/share/sddm/themes/sddm-astronaut-theme
+	echo "${password}" | sudo -S cp /usr/share/sddm/themes/sddm-astronaut-theme/Fonts/* /usr/share/fonts/
 	echo "${password}" | sudo -S sed -i "s/FullBlur=\"false\"/FullBlur=\"true\"/g" /usr/share/sddm/themes/sddm-astronaut-theme/theme.conf
 	echo "${password}" | sudo -S sed -i "s/PartialBlur=\"true\"/PartialBlur=\"false\"/g" /usr/share/sddm/themes/sddm-astronaut-theme/theme.conf
 	echo "${password}" | sudo -S sed -i "s/BlurRadius=\"80\"/BlurRadius=\"20\"/g" /usr/share/sddm/themes/sddm-astronaut-theme/theme.conf
@@ -93,16 +103,16 @@ function Installi3() {
 }
 
 function InstallAuthenticationAgent() {
-    echo "${password}" | sudo -S pacman -S polkit-gnome
+	echo "${password}" | sudo -S pacman -S polkit-gnome
 }
 
 function InstallNotificationServer() {
 	echo -e "${BYellow}[ * ]Notification Server for window manager${End_Colour}"
-    echo "${password}" | sudo -S pacman -S libnotify notification-daemon
-    cur_dir=pwd
-    cd /PostInstall
-    echo "${password}" | sudo -S cp org.freedesktop.Notification.service /usr/share/dbus-1/services/
-    cd ${cur_dir}
+	echo "${password}" | sudo -S pacman -S libnotify notification-daemon
+	cur_dir=pwd
+	cd /PostInstall
+	echo "${password}" | sudo -S cp org.freedesktop.Notification.service /usr/share/dbus-1/services/
+	cd ${cur_dir}
 }
 
 function InstallNeovim() {
@@ -112,7 +122,7 @@ function InstallNeovim() {
 
 function CopyConfigFiles() {
 	echo -e "${BYellow}[ * ]Copying relevant config files${End_Colour}"
-    cur_dir=pwd
+	cur_dir=pwd
 	cd /home/${username}
 	git clone https://github.com/metalyos/dotfiles.git
 	cd dotfiles
@@ -120,30 +130,31 @@ function CopyConfigFiles() {
 
 	cd /PostInstall
 	sudo cp 40-libinput.conf /etc/X11/xorg.conf.d/
-    cd ${cur_dir}
+	cd ${cur_dir}
 }
 
 function SetupGitConfig() {
-    # TODO: get values in installation script
-    git config --global user.email "metalyos@gmail.com"
-    git config --global user.name "MetalYos"
+	# TODO: get values in installation script
+	git config --global user.email "metalyos@gmail.com"
+	git config --global user.name "MetalYos"
 }
 
 function Main() {
 	echo -e "${BYello}[ * ]Entered user_post_install script with arguments ${username} ${password}${End_Colour}"
 	InstallAurHelper
-	InstallAdditionalPackages
-    EnableAutoUsbMounting
+	EnableAutoUsbMounting
 	InstallPipewireAudio
 	InstallDisplayServer
 	InstallDisplayManager
 	Installi3
-    InstallAuthenticationAgent
-    InstallNotificationServer
+	InstallAuthenticationAgent
+	InstallNotificationServer
+	InstallAdditionalPackages
+	InstallAdditionalAurPackages
 	InstallNeovim
 	CopyConfigFiles
 
-    SetupGitConfig
+	SetupGitConfig
 }
 
 Main "$@"
